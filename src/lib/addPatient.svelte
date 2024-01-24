0f14257bed9e000b29528dc643474b1da70c37ad
Evrim <ehotamis@gmail.com>
2024-01-24 15:52:25 +0000

    rebase addpt

---
+++ src/lib/addPatient.svelte
@@ -0,0 +1,9 @@
+  <script></script>
+  <form>
+    <label for="naam">Naam</label>
+    <input type="text" id="naam" bind:value={naam} />
+    <label for="opnamedatum">Opnamedatum</label>
+    <input type="date" id="opnamedatum" bind:value={opnamedatum} />
+    <button on:click={addPatient} abort="submit">Toevoegen</button>
+    <p>{status}</p>
+  </form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -68,12 +68,5 @@
         {/if}
     </tbody>
   </table>
-  <!-- <form>
-    <label for="naam">Naam</label>
-    <input type="text" id="naam" bind:value={naam} />
-    <label for="opnamedatum">Opnamedatum</label>
-    <input type="date" id="opnamedatum" bind:value={opnamedatum} />
-    <button on:click={addPatient} abort="submit">Toevoegen</button>
-    <p>{status}</p>
-  </form> -->
+
 </div>
