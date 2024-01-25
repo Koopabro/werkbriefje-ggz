936b9aaef5d4790995977ff6b884ff4129645663
Evrim <ehotamis@gmail.com>
2024-01-25 10:06:45 +0000

    material ui implementation

--- src/lib/addPatient.svelte
+++ src/lib/addPatient.svelte
@@ -1,5 +1,40 @@
 <script>
 	import { db } from '$lib/db';
+	import FormField from '@smui/form-field';
+	import SegmentedButton, { Segment } from '@smui/segmented-button';
+	import Button from '@smui/button';
+	import { Label } from '@smui/common';
+	import Select, { Option } from '@smui/select';
+	import Snackbar, { Actions } from '@smui/snackbar';
+
+	//snackbar
+	let reason = 'nothing yet';
+	let snackbar = Snackbar;
+	// function handleClosed(e: CustomEvent<{ reason: string | undefined }>) {
+	// 	reason = e.detail.reason ?? 'Undefined.';
+	// }
+
+	const handleClosed = () => {};
+
+	//form
+	let titelkeuze = ['WvGGZ', 'WFZ'];
+	let fasekeuze = [
+		'0a',
+		'0b',
+		'1',
+		'2',
+		'3a',
+		'3b',
+		'3c',
+		'3d',
+		'4a',
+		'4b',
+		'4c',
+		'4d',
+		'5a',
+		'5b'
+	];
+
 	let naam = '';
 	const datum = new Date();
 	let opnamedatum = datum.toISOString().slice(0, 10);
@@ -24,9 +59,10 @@
 		brief: false
 	};
 	let status = '';
+	let laatsteId = '';
 
 	const addPatient = async () => {
-		const id = await db.fhic.add({
+		id = await db.fhic.add({
 			naam: naam,
 			opnamedatum: opnamedatum,
 			titel: titel,
@@ -37,35 +73,46 @@
 		});
 		status = `Patient toegevoegd met id ${id}`;
 	};
+
+	const verwijderPatient = async (id) => {
+		await db.fhic.delete(id);
+	};
+
+	const deleteAll = async () => {
+		await db.fhic.clear();
+	};
 </script>
 
-<form>
+<FormField>
 	<label for="naam">Naam</label>
 	<input type="text" id="naam" bind:value={naam} />
 	<label for="opnamedatum">Opnamedatum</label>
 	<input type="date" id="opnamedatum" bind:value={opnamedatum} />
 	<label for="titel">Titel</label>
-	<select name="titel" id="titel" bind:value={titel}>
-		<option value="WvGGZ">WvGGZ</option>
-		<option value="WFZ">WFZ</option>
-	</select>
+	<SegmentedButton segments={titelkeuze} let:segment singleSelect bind:titel>
+		<!-- Note: the `segment` property is required! -->
+		<Segment {segment}>
+			<Label>{segment}</Label>
+		</Segment>
+	</SegmentedButton>
 	<label for="fase">Fase</label>
-	<select name="fase" id="fase" bind:value={fase}>
-		<option value="0a">0a</option>
-		<option value="0b">0b</option>
-		<option value="1">1</option>
-		<option value="2">2</option>
-		<option value="3a">3a</option>
-		<option value="3b">3b</option>
-		<option value="3c">3c</option>
-		<option value="3d">3d</option>
-		<option value="4a">4a</option>
-		<option value="4b">4b</option>
-		<option value="4c">4c</option>
-		<option value="4d">4d</option>
-		<option value="5a">5a</option>
-		<option value="5b">5b</option>
-	</select>
-	<button on:click={addPatient} abort="submit">Toevoegen</button>
-	<p>{status}</p>
-</form>
+	<Select name="fase" id="fase" bind:value={fase}>
+		{#each fasekeuze as fasen}
+			<Option value={fasen}>{fasen}</Option>
+		{/each}
+	</Select>
+	<button
+		on:click={async () => {
+			await addPatient;
+			snackbar.open();
+		}}
+		abort="submit">Toevoegen</button
+	>
+	<Snackbar leading bind:this={snackbar}>
+		<Label>{status}</Label>
+		<Actions>
+			<Button on:click={verwijderPatient(laatsteId)}>Ongedaan</Button>
+		</Actions>
+	</Snackbar>
+	<button on:click={deleteAll}>Verwijder alles</button>
+</FormField>
